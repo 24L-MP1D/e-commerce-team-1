eff3346b13db49b41ac6e74413350064f58e171d
Zolookor2004 <zolookorzoloo@gmail.com>
2024-09-12 17:18:55 +0800

    minor pull

---
+++ src/app/category/page.tsx
@@ -0,0 +1,36 @@
+const category = [
+  {
+    name: "Малгай",
+    param: "hat"
+  },
+  {
+    name: "Усны сав",
+    param: "bottle"
+  },
+  {
+    name: "T-shirt",
+    param: "tshirt"
+  },
+  {
+    name: "Малгай"
+  },
+  {
+    name: "Hoodie"
+  },
+  {
+    name: "Tee"
+  },
+  {
+    name: "Цүнх"
+  }
+];
+
+export default function Home() {
+  return (
+    <div className="w-full max-w-[1040px] flex gap-5">
+      <div className="flex flex-col gap-4">
+        <span className="font-bold">Ангилал</span>
+      </div>
+    </div>
+  );
+}
