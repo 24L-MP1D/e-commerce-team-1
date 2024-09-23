eb5a44f2cbc8a7b655fd4f78cc3a655f0fa8d413
Zoljargal2004 <zolookorzoloo@gmail.com>
2024-09-23 13:15:19 +0800

    ADDS Cors

--- src/app/services/Product.ts
+++ src/app/services/Product.ts
@@ -36,11 +36,23 @@ export const getProductByCategory = async (
   size: string[]
 ) => {
   try {
-    console.log(category, size);
-    const res = await fetch(`/Product.json`);
-    const data = res.json();
+    const res = await fetch(`http://localhost:5000/product/list`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ categoryId: category, size: size }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    console.log(data);
     return data;
   } catch (error) {
     console.error("FrontEnd error: ", error);
+    return [];
   }
 };
