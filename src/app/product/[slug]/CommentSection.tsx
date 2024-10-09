d3ac38366b979fd7bb221f24398a1de6290d40ed
Zolookor2004 <zolookorzoloo@gmail.com>
2024-10-09 19:49:30 +0800

    whateever

--- src/app/product/[slug]/CommentSection.tsx
+++ src/app/product/[slug]/CommentSection.tsx
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import StarRating from "./StarRating";
+import {
+  deleteComment,
+  fetchComments,
+  postComment
+} from "@/app/services/comment";
+import { Edit, Star, Trash } from "lucide-react";
+import { Dialog } from "@/components/ui/dialog";
+import EditComment from "@/components/editCommentDialog";
+
+const { jwtDecode } = require("jwt-decode");
 
 interface Review {
-  reviewer: string;
+  userId: {
+    _id: string;
+    userName: string;
+  };
   comment: string;
   rating: number;
 }
 
-const CommentSection: React.FC = () => {
+const CommentSection = ({ slug }: { slug: string }) => {
   const [isVisible, setIsVisible] = useState(false);
   const [commentText, setCommentText] = useState("");
   const [rating, setRating] = useState(0);
   const [comments, setComments] = useState<Review[]>([]);
-
+  const [edit, setEdit] = useState(false);
+  const token = localStorage.getItem("Authorization") || "";
+  let total = 0;
+  comments.map((comment) => (total += comment.rating));
+  const avg = total / comments.length;
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
   };
@@ -28,28 +45,47 @@ const CommentSection: React.FC = () => {
     setRating(newRating);
   };
 
-  const handleSubmitComment = () => {
+  const getComments = async () => {
+    setComments(await fetchComments(slug));
+  };
+
+  const handleSubmitComment = async () => {
     if (commentText.trim() === "") {
       alert("Comment cannot be empty.");
       return;
     }
 
-    const newComment: Review = {
-      reviewer: "Current User",
-      comment: commentText,
-      rating: rating,
-    };
+    await postComment(slug, rating, commentText);
 
-    setComments([newComment, ...comments]);
+    getComments();
 
     setCommentText("");
     setRating(0);
   };
 
+  useEffect(() => {
+    getComments();
+  }, []);
+
   return (
     <div className="text-sm">
-      <div className="flex gap-4 mt-5">
-        <p className="text-gray-900">Үнэлгээ</p>
+      <EditComment
+        open={edit}
+        setOpen={() => {
+          setEdit(!edit);
+        }}
+        reset={getComments}
+        slug={slug}
+      />
+      <div className="flex gap-4 mt-5 items-center">
+        <div>
+          <p className="text-gray-900">Үнэлгээ</p>
+          <span className="flex gap-2 items-center">
+            <span>{avg}</span>
+            <Star className="text-yellow-500" fill={`#eab308`} />
+          </span>
+        </div>
+
         <button onClick={toggleVisibility} className="text-blue-600 underline">
           {isVisible ? "бүгдийг хураах" : "бүгдийг харах"}
         </button>
@@ -63,14 +99,33 @@ const CommentSection: React.FC = () => {
                 {comments.map((review, index) => (
                   <div
                     key={index}
-                    className="mb-4 p-4 border border-gray-200 rounded"
+                    className="mb-4 p-4 border border-gray-200 rounded relative"
                   >
-                    <p className="font-semibold">{review.reviewer}</p>
+                    <p className="font-semibold">{review.userId.userName}</p>
                     <StarRating
                       rating={review.rating}
                       onRatingChange={() => {}}
                     />
                     <p className="mt-2">{review.comment}</p>
+                    {token && jwtDecode(token)?.id == review.userId._id && (
+                      <div className="absolute right-3 top-4 flex gap-2 z-50">
+                        <button
+                          onClick={() => {
+                            setEdit(!edit);
+                          }}
+                        >
+                          <Edit size={18} />
+                        </button>
+                        <button
+                          onClick={async () => {
+                            await deleteComment(slug);
+                            getComments();
+                          }}
+                        >
+                          <Trash size={18} />
+                        </button>
+                      </div>
+                    )}
                   </div>
                 ))}
               </div>
@@ -103,7 +158,8 @@ const CommentSection: React.FC = () => {
 };
 
 export default CommentSection;
-{/* <div className="flex flex-col gap-6 ">
+{
+  /* <div className="flex flex-col gap-6 ">
 <p className="text-3xl font-bold">Холбоотой бараа</p>
 <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-[21px]">
   {products.map((product) => (
@@ -119,4 +175,5 @@ export default CommentSection;
     ></div>
   ))}
 </div>
-</div> */}
+</div> */
+}
